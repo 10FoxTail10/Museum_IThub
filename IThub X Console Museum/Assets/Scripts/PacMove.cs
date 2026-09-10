a884ff851da717ddcc7236e19207e69a163ea6e7
FoxTail <102622522+10FoxTail10@users.noreply.github.com>
2026-09-10 18:49:09 +0300

    Изменения в управлении

--- IThub X Console Museum/Assets/Scripts/PacMove.cs
+++ IThub X Console Museum/Assets/Scripts/PacMove.cs
@@ -2,65 +2,85 @@
 
 public class PacMove : MonoBehaviour
 {
-    [Header("Прочти первый комент")]
     [Header("Настройки скорости")]
     [SerializeField] private float moveSpeed = 5f;
+    [Header("Способ управления")]
+    [Tooltip("true - клавиатура и мышка, а false - геймпад")]
+    [SerializeField] private bool inputDevise = true;
+    // true - клавиатура и мышка, а false - геймпад.
 
     private Rigidbody2D rb;
     private Vector2 movement;
-    
-    // Переменная для отслеживания направления. 
-    // false = смотрит влево (как ты и просил), true = вправо
-    private bool facingRight = false; 
+    private bool facingRight = false;
+    // false = смотрит влево (клавиша "D"), true = вправо (клавиша "A").
 
     private void Awake()
     {
-        //Rigidbody сделает движения более плавными чем transform
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        // сбрасыаем предидущее
-        movement = Vector2.zero;
-
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-            movement.y = 1f;
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-            movement.y = -1f;
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            movement.x = 1f;
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            movement.x = -1f;
-
-        // убираю ускорение по диагонали
-        if (movement.magnitude > 1f)
-            movement.Normalize();
-
-        // --- ЛОГИКА ПОВОРОТА СПРАЙТА ---
-        // Если нажали D (x > 0), но персонаж смотрит влево (!facingRight) -> поворачиваем
-        if (movement.x > 0 && !facingRight)
-            Flip();
-        // Если нажали A (x < 0), но персонаж смотрит вправо (facingRight) -> поворачиваем
-        else if (movement.x < 0 && facingRight)
-            Flip();
-        // --------------------------------
+        InputController();
     }
 
     private void FixedUpdate()
     {
-        // Применяем движение к Rigidbody2D
         rb.velocity = movement * moveSpeed;
     }
 
-    // Метод зеркального отражения
+    private void InputController()
+    {
+        movement = Vector2.zero;
+
+        if (inputDevise == true)
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                movement.y = 1f;
+            }
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                movement.y = -1f;
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                movement.x = 1f;
+            }
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                movement.x = -1f;
+            }
+        }
+
+        if (inputDevise == false)
+        {
+            movement = Vector2.zero;
+
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+        }
+
+        if (movement.magnitude > 1f)
+        {
+            movement.Normalize();
+        }
+
+        if (movement.x > 0 && facingRight == false)
+        {
+            Flip();
+        }
+        if (movement.x < 0 && facingRight == true)
+        {
+            Flip();
+        }
+    }
+
     private void Flip()
     {
-        // Меняем булево значение на противоположное
         facingRight = !facingRight;
         
-        // Берем текущий масштаб, инвертируем ось X и применяем обратно
-        Vector3 scale = transform.localScale;
+        Vector2 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
     }
